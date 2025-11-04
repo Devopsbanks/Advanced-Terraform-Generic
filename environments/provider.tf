834e50f44fa3d843c0fce8d0ba6d063be90f667c
anjali <anjali@gmail.com>
2025-11-04 17:00:21 +0530

    generic terrfaorm module for Monolithic

---
+++ environments/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+
+  }
+  subscription_id = "239a3096-3fb0-4f7c-8060-6127b0f8808e"
+}
