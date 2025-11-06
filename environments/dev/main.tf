73b54c406aa035c944d34c2aa4ffbb3392d41af3
Devopsbanks <desiboys85@outlook.com>
2025-11-06 08:38:47 +0530

    tfvars added for all 3 environment

---
+++ environments/dev/main.tf
@@ -0,0 +1,33 @@
+module "resource_group" {
+  source = "../modules/azurerm_rg"
+  rgs    = var.rgs
+}
+
+module "virtual_network" {
+  depends_on = [module.resource_group]
+  source     = "../modules/azurerm_vnet"
+  vnets      = var.vnets
+}
+module "public_ip" {
+  depends_on = [module.resource_group]
+  source     = "../modules/azurerm_public_ip"
+  pips       = var.pips
+}
+
+module "kv" {
+  depends_on = [module.resource_group]
+  source     = "../modules/azurerm_key_vault"
+  keys       = var.keys
+}
+
+module "secret" {
+  depends_on = [module.kv]
+  source     = "../modules/azurerm_kv_secret"
+  secrets    = var.secrets
+}
+
+module "vm" {
+  depends_on = [module.resource_group, module.public_ip, module.virtual_network, module.kv, module.secret]
+  source     = "../modules/azurerm_vm"
+  vms        = var.vms
+}
